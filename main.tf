c12d77ac1a4127ea013bd6118a94feca429ed8e0
Donghwan-Lim <ldh0727@insideinfo.co.kr>
2023-07-11 10:28:38 +0900

    Create Subnets

--- main.tf
+++ main.tf
@@ -23,9 +23,15 @@ locals {
   }
 }
 
+### AWS Data AZ List ###
+data "aws_availability_zones" "all" {}
+
 ### AWS VPC ###
 resource "aws_vpc" "vpc01" {
   cidr_block = "10.10.10.0/24"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+
   tags = (merge(local.common-tags, tomap({
     Name = "vpc01"
     resource = "aws_vpc"
@@ -34,6 +40,9 @@ resource "aws_vpc" "vpc01" {
 
 resource "aws_vpc" "vpc02" {
   cidr_block = "10.10.20.0/24"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+
   tags = (merge(local.common-tags, tomap({
     Name = "vpc02"
     resource = "aws_vpc"
@@ -41,3 +50,83 @@ resource "aws_vpc" "vpc02" {
 }
 
 
+### AWS Subnet ###
+resource "aws_subnet" "vpc01-sbn-pub-01" {
+  vpc_id = aws_vpc.vpc01.id
+  availability_zone = data.aws_availability_zones.all.name[0]
+  cidr_block = "10.10.10.0/26"
+  tags = (merge(local.common-tags, tomap ({
+    Name = "vpc01-pub01-sbn"
+    resource = "aws_subnet"
+  })))
+}
+
+resource "aws_subnet" "vpc01-sbn-pub-02" {
+  vpc_id = aws_vpc.vpc01.id
+  availability_zone = data.aws_availability_zones.all.name[2]
+  cidr_block = "10.10.10.64/26"
+  tags = (merge(local.common-tags, tomap ({
+    Name = "vpc01-pub02-sbn"
+    resource = "aws_subnet"
+  })))
+}
+
+resource "aws_subnet" "vpc01-sbn-priv-01" {
+  vpc_id = aws_vpc.vpc01.id
+  availability_zone = data.aws_availability_zones.all.name[0]
+  cidr_block = "10.10.10.128/26"
+  tags = (merge(local.common-tags, tomap ({
+    Name = "vpc01-priv01-sbn"
+    resource = "aws_subnet"
+  })))
+}
+
+resource "aws_subnet" "vpc01-sbn-priv-02" {
+  vpc_id = aws_vpc.vpc01.id
+  availability_zone = data.aws_availability_zones.all.name[2]
+  cidr_block = "10.10.10.192/26"
+  tags = (merge(local.common-tags, tomap ({
+    Name = "vpc01-priv02-sbn"
+    resource = "aws_subnet"
+  })))
+}
+
+resource "aws_subnet" "vpc02-sbn-pub-01" {
+  vpc_id = aws_vpc.vpc02.id
+  availability_zone = data.aws_availability_zones.all.name[0]
+  cidr_block = "10.10.20.0/26"
+  tags = (merge(local.common-tags, tomap ({
+    Name = "vpc02-pub01-sbn"
+    resource = "aws_subnet"
+  })))
+}
+
+resource "aws_subnet" "vpc02-sbn-pub-02" {
+  vpc_id = aws_vpc.vpc02.id
+  availability_zone = data.aws_availability_zones.all.name[2]
+  cidr_block = "10.10.20.64/26"
+  tags = (merge(local.common-tags, tomap ({
+    Name = "vpc02-pub02-sbn"
+    resource = "aws_subnet"
+  })))
+}
+
+resource "aws_subnet" "vpc02-sbn-priv-01" {
+  vpc_id = aws_vpc.vpc02.id
+  availability_zone = data.aws_availability_zones.all.name[0]
+  cidr_block = "10.10.20.128/26"
+  tags = (merge(local.common-tags, tomap ({
+    Name = "vpc02-priv01-sbn"
+    resource = "aws_subnet"
+  })))
+}
+
+resource "aws_subnet" "vpc02-sbn-priv-02" {
+  vpc_id = aws_vpc.vpc02.id
+  availability_zone = data.aws_availability_zones.all.name[2]
+  cidr_block = "10.10.20.192/26"
+  tags = (merge(local.common-tags, tomap ({
+    Name = "vpc02-priv02-sbn"
+    resource = "aws_subnet"
+  })))
+}
--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "vpc01_id" {
     value = aws_vpc.vpc01.id
     description = "INSIDE_AWS_LABNET_VPC_01_ID(10.10.10.0/24)"
 }
+
+output "vpc02_id" {
+    value = aws_vpc.vpc02.id
+    description = "INSIDE_AWS_LABNET_VPC_02_ID(10.10.20.0/24)"
+}
