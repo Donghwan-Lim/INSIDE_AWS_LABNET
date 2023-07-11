992be3badeccb400fec04c4ec92bdf09dab444a2
Donghwan-Lim <ldh0727@insideinfo.co.kr>
2023-07-11 10:05:32 +0900

    Create_VPCs

--- main.tf
+++ main.tf
@@ -9,8 +9,35 @@ terraform {
   }
 }
 
-
 ### AWS Provider Info ###
 provider "aws" {
     region = var.region  
 }
+
+### AWS COMMON TAGS ###
+locals {
+  common-tags = {
+    author = "DonghwanLim"
+    system = "LAB"
+    Environment = "INSIDE__AWS_NETWORK"
+  }
+}
+
+### AWS VPC ###
+resource "aws_vpc" "vpc01" {
+  cidr_block = "10.10.10.0/24"
+  tags = (merge(local.common-tags, tomap({
+    Name = "vpc01"
+    resource = "aws_vpc"
+  })))
+}
+
+resource "aws_vpc" "vpc02" {
+  cidr_block = "10.10.20.0/24"
+  tags = (merge(local.common-tags, tomap({
+    Name = "vpc02"
+    resource = "aws_vpc"
+  })))
+}
+
+
---
+++ output.tf
@@ -0,0 +1,4 @@
+output "vpc01_id" {
+    value = aws_vpc.vpc01.id
+    description = "INSIDE_AWS_LABNET_VPC_01_ID(10.10.10.0/24)"
+}
