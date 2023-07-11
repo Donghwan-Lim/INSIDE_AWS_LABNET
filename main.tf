9a62959877813e8986ba6cb90afcc6c2a205d249
Donghwan-Lim <ldh0727@insideinfo.co.kr>
2023-07-11 11:09:28 +0900

    change_default_route_Table_id_each_aws_vpc

--- main.tf
+++ main.tf
@@ -31,6 +31,7 @@ resource "aws_vpc" "vpc01" {
   cidr_block = "10.10.10.0/24"
   enable_dns_hostnames = true
   enable_dns_support = true
+  default_route_table_id = aws_route_table.vpc01-rt-private.id
 
   tags = (merge(local.common-tags, tomap({
     Name = "vpc01"
@@ -42,6 +43,7 @@ resource "aws_vpc" "vpc02" {
   cidr_block = "10.10.20.0/24"
   enable_dns_hostnames = true
   enable_dns_support = true
+  default_route_table_id = aws_route_table.vpc02-rt-private.id
 
   tags = (merge(local.common-tags, tomap({
     Name = "vpc02"
@@ -183,4 +185,42 @@ resource "aws_route_table_association" "vpc01-rt-priv-to-sbn-priv01" {
 resource "aws_route_table_association" "vpc01-rt-priv-to-sbn-priv02" {
   route_table_id = aws_route_table.vpc01-rt-private.id
   subnet_id = aws_subnet.vpc01-sbn-priv-02.id
+}
+
+resource "aws_route_table" "vpc02-rt-public" {
+  vpc_id = aws_vpc.vpc02.id
+
+  tags = (merge(local.common-tags, tomap ({
+    Name = "VPC02-PUBLIC-RT"
+    resource = "aws_route_table"
+  })))
+}
+
+resource "aws_route_table_association" "vpc02-rt-pub-to-sbn-pub01" {
+  route_table_id = aws_route_table.vpc02-rt-public.id
+  subnet_id = aws_subnet.vpc02-sbn-pub-01.id
+}
+
+resource "aws_route_table_association" "vpc02-rt-pub-to-sbn-pub02" {
+  route_table_id = aws_route_table.vpc02-rt-public.id
+  subnet_id = aws_subnet.vpc02-sbn-pub-02.id
+}
+
+resource "aws_route_table" "vpc02-rt-private" {
+  vpc_id = aws_vpc.vpc02.id
+
+  tags = (merge(local.common-tags, tomap ({
+    Name = "VPC02-PRIVATE-RT"
+    resource = "aws_route_table"
+  })))
+}
+
+resource "aws_route_table_association" "vpc02-rt-priv-to-sbn-priv01" {
+  route_table_id = aws_route_table.vpc02-rt-private.id
+  subnet_id = aws_subnet.vpc02-sbn-priv-01.id
+}
+
+resource "aws_route_table_association" "vpc02-rt-priv-to-sbn-priv02" {
+  route_table_id = aws_route_table.vpc02-rt-private.id
+  subnet_id = aws_subnet.vpc02-sbn-priv-02.id
 }
