8a496f8b7fe22512161cf0229534355c77a8ab1a
Donghwan-Lim <ldh0727@insideinfo.co.kr>
2023-10-11 10:45:20 +0900

    Modify Subnet Tags For EKS Fixed Error

--- main.tf
+++ main.tf
@@ -74,10 +74,10 @@ resource "aws_subnet" "vpc01-sbn-pub-01" {
   availability_zone = data.aws_availability_zones.all.names[0]
   cidr_block        = "10.10.10.0/26"
   tags = (merge(local.common-tags, tomap({
-    Name                                      = "vpc01-pub01-sbn"
-    resource                                  = "aws_subnet"
-    kubernetes.io / cluster / INSIDE_learnk8s = "shared"
-    kubernetes.io / role / elb                = "1"
+    Name                                   = "vpc01-pub01-sbn"
+    resource                               = "aws_subnet"
+    "kubernetes.io/cluster/INSIDE_learnk8" = "shared"
+    "kubernetes.io/role/elb"               = "1"
   })))
 }
 
@@ -86,10 +86,10 @@ resource "aws_subnet" "vpc01-sbn-pub-02" {
   availability_zone = data.aws_availability_zones.all.names[2]
   cidr_block        = "10.10.10.64/26"
   tags = (merge(local.common-tags, tomap({
-    Name                                      = "vpc01-pub02-sbn"
-    resource                                  = "aws_subnet"
-    kubernetes.io / cluster / INSIDE_learnk8s = "shared"
-    kubernetes.io / role / elb                = "1"
+    Name                                   = "vpc01-pub02-sbn"
+    resource                               = "aws_subnet"
+    "kubernetes.io/cluster/INSIDE_learnk8" = "shared"
+    "kubernetes.io/role/elb"               = "1"
   })))
 }
 
@@ -98,10 +98,10 @@ resource "aws_subnet" "vpc01-sbn-priv-01" {
   availability_zone = data.aws_availability_zones.all.names[0]
   cidr_block        = "10.10.10.128/26"
   tags = (merge(local.common-tags, tomap({
-    Name                                      = "vpc01-priv01-sbn"
-    resource                                  = "aws_subnet"
-    kubernetes.io / cluster / INSIDE_learnk8s = "shared"
-    kubernetes.io / role / internal-elb       = "1"
+    Name                                    = "vpc01-priv01-sbn"
+    resource                                = "aws_subnet"
+    "kubernetes.io/cluster/INSIDE_learnk8s" = "shared"
+    "kubernetes.io/role/internal-elb"       = "1"
   })))
 }
 
@@ -110,10 +110,10 @@ resource "aws_subnet" "vpc01-sbn-priv-02" {
   availability_zone = data.aws_availability_zones.all.names[2]
   cidr_block        = "10.10.10.192/26"
   tags = (merge(local.common-tags, tomap({
-    Name                                      = "vpc01-priv02-sbn"
-    resource                                  = "aws_subnet"
-    kubernetes.io / cluster / INSIDE_learnk8s = "shared"
-    kubernetes.io / role / internal-elb       = "1"
+    Name                                    = "vpc01-priv02-sbn"
+    resource                                = "aws_subnet"
+    "kubernetes.io/cluster/INSIDE_learnk8s" = "shared"
+    "kubernetes.io/role/internal-elb"       = "1"
   })))
 }
 
