a61e97fa9c9bee5e6239db77dd91924d09d96a4a
Donghwan-Lim <ldh0727@insideinfo.co.kr>
2023-11-06 14:09:29 +0900

    vpc1-pub and vpc2-priv setting

--- output.tf
+++ output.tf
@@ -17,7 +17,7 @@ output "vpc01_public_subnet_02_id" {
   value       = aws_subnet.vpc01-sbn-pub-02.id
   description = "INSIDE_AWS_LABNET_VPC_01_SUBNET_PUBLIC_02_ID(10.10.10.64/26)"
 }
-
+/*
 output "vpc01_private_subnet_01_id" {
   value       = aws_subnet.vpc01-sbn-priv-01.id
   description = "INSIDE_AWS_LABNET_VPC_01_SUBNET_PRIVATE_01_ID(10.10.10.128/26)"
@@ -37,7 +37,7 @@ output "vpc02_public_subnet_02_id" {
   value       = aws_subnet.vpc02-sbn-pub-02.id
   description = "INSIDE_AWS_LABNET_VPC_02_SUBNET_PUBLIC_02_ID(10.10.20.64/26)"
 }
-
+*/
 output "vpc02_private_subnet_01_id" {
   value       = aws_subnet.vpc02-sbn-priv-01.id
   description = "INSIDE_AWS_LABNET_VPC_02_SUBNET_PRIVATE_01_ID(10.10.20.128/26)"
