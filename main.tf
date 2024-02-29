c17d2ec1bb02108d04d0b8de5c26fd2ce1ec3c72
Donghwan-Lim <ldh0727@insideinfo.co.kr>
2024-02-29 10:19:53 +0900

    transit gw default rt setup change resource block

--- main.tf
+++ main.tf
@@ -326,22 +326,23 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "tgw01-attach-vpc02" {
   transit_gateway_id = aws_ec2_transit_gateway.tgw01.id
   vpc_id             = aws_vpc.vpc02.id
 }
-
+/*
 resource "aws_ec2_transit_gateway_route_table" "tgw01-routetable" {
-  /*
+  
   transit_gateway_id = aws_ec2_transit_gateway.tgw01.id
 
   tags = (merge(local.common-tags, tomap({
     Name     = "TGW_Route_Table"
     resource = "aws_transit_gateway_route_table"
   })))
-  */
 }
-
+*/
+/*
 import {
   to = aws_ec2_transit_gateway_route_table.tgw01-routetable
   id = "tgw-rtb-08133dced42ca6b30"
 }
+*/
 /*
 resource "aws_ec2_transit_gateway_route" "tgw01-route" {
   destination_cidr_block         = "0.0.0.0/0"
